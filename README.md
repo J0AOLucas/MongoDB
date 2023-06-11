9a3f7e5113644b355e15df0248642f959bea3d5b
Lucas <lucasrpg.silva@gmail.com>
2023-06-11 19:26:04 -0300

    new files

---
+++ README.md
@@ -0,0 +1,14 @@
+# MongoDB
+
+## Trabalho final
+
+Loja de acessórios 
+
+[Link do Banco de dados](https://data.world/crowdflower/wearable-technology-database) 
+
+Esquema de paginas 
+<img src="Flowchart.jpg">
+
+Alunos :
+ - Ana Rachel 
+ - João Lucas
---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+from pymongo import MongoClient
+from bson import ObjectId
+
+client = MongoClient('mongodb://localhost:27017')
+db = client['watches']
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def home():
+    collection = db['products']
+    documentos = collection.find().limit(5)
+    
+    return render_template("home.html", documentos=documentos)
+
+@app.route("/produtos.html", methods=['GET'])
+def produtos():
+    collection = db['products']
+    documentos = collection.find().limit(80)
+    
+    return render_template("produtos.html", documentos=documentos)
+
+@app.route("/adicionar.html")
+def adicionar():
+    return render_template("adicionar.html")
+
+@app.route("/produto.html/<id>", methods=['GET'])
+def produto(id):
+    collection = db['products']
+    documentos = collection.find_one({'_id': ObjectId(id)})
+    
+    return render_template("produto.html", documentos=documentos)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    

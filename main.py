a62277f1e8fbd9453a512a1a664d819ba7dad7c6
AnaRachel <anaracheleng@gmail.com>
2023-06-12 18:15:00 -0300

    add route

--- main.py
+++ main.py
@@ -1,36 +1,62 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect , url_for, render_template
 from pymongo import MongoClient
 from bson import ObjectId
+import logging
 
-client = MongoClient('mongodb://localhost:27017')
-db = client['watches']
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
+client = MongoClient('mongodb://localhost:27017')
+db = client['watches']
+collection = db['products']
+
 @app.route("/", methods=['GET'])
 def home():
-    collection = db['products']
     documentos = collection.find().limit(5)
-    
     return render_template("home.html", documentos=documentos)
+
 
 @app.route("/produtos.html", methods=['GET'])
 def produtos():
-    collection = db['products']
     documentos = collection.find().limit(80)
-    
     return render_template("produtos.html", documentos=documentos)
 
 
-@app.route("/adicionar.html")
+@app.route("/adicionar.html", methods=['GET', 'POST'])
 def adicionar():
-    return render_template("adicionar.html")
+    if request.method == "GET":
+        return render_template("adicionar.html")
+    else: 
+        Name = request.form['nome']
+        Company = request.form['marca']
+        Category = request.form['categoria']
+        Price = request.form['preco']
+        Localizacao = request.form['localizacao']
+        Imagem = request.form['imagem']
+        Observations = request.form['obs']
+    
+        new_acessory ={
+            "Name" : Name,
+            "Company" : {
+                "Name" : Company} ,
+            "Category" : Category,
+            "Price" : Price,
+            "Body" : {
+                "Location" : Localizacao} ,
+            "Imagem" : Imagem,
+            "Observations" : Observations
+        }
+        
+        db.products.insert_one(new_acessory)
+        logging.info(f"Produto inserido com sucesso: {new_acessory}")
+        
+        return redirect("/")
+
 
 @app.route("/produto.html/<id>", methods=['GET'])
 def produto(id):
-    collection = db['products']
     documentos = collection.find_one({'_id': ObjectId(id)})
-    
     return render_template("produto.html", documentos=documentos)
 
 
